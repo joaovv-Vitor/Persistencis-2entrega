2f1f10535e1c71832b0222311861eaa88debdf5f
Kaua Pimentel <carloskkauapimentel@gmail.com>
2025-01-12 19:55:05 -0300

    modelando entidade publicacao, kaua

--- models/publicacao.py
+++ models/publicacao.py
@@ -1,8 +1,17 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+from datetime import datetime 
+from models import Usuario, AlbumPub
+from typing import List
 
 class Publicacao(SQLModel, table= True):
     __tablename__= 'Publicacoes'
-    id_pub: int = Field(default=None, primary_key=True)
+    id_pub: int | None = Field(default=None, primary_key=True)
+    id_autor: int | None = Field(foreign_key="usuario.id_user")
     legenda: str | None = Field(default=None)
-    likes: int
+    likes: int = Field(default=0)
     id_autor: int 
+    caminho_imagem: str
+    data_criacao: datetime
+    
+    autor: Usuario = Relationship(back_populates='Publicacao')
+    albuns: List['AlbumPub'] = Relationship(back_populates='Publicacao')
