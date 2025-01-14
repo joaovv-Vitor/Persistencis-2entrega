56540a81296a79715f0cab81976562dfda1384f0
João Vitor <163120711+joaovv-Vitor@users.noreply.github.com>
2025-01-14 15:22:08 -0300

    Comecando o model perfil e alterando o nome de usuarios => perfis

---
+++ models/Perfis.py
@@ -0,0 +1,15 @@
+from sqlmodel  import SQLModel, Field, Relationship
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .albuns import Albuns
+    from .publicacao import Publicacao
+
+
+class PerfilBase(SQLModel):
+    id: int | None = Field(default = None, primary_key = True)
+    email: str = Field(unique = True)
+    nome: str
+    bio: str
+    
+
