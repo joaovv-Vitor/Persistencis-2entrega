c28c9045f817cd13ba6f49e8827a9b1c5c17fd85
Kaua Pimentel <carloskkauapimentel@gmail.com>
2025-01-15 13:01:02 -0300

    adicionando integridade referencial para sqlite, kaua

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 import logging
 import os
+import sqlite3
 
 from dotenv import load_dotenv
+from sqlalchemy import Engine, event
 from sqlmodel import Session, SQLModel, create_engine
 
 # carregar as variaveis do arquivo .env
@@ -12,15 +14,22 @@ logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 # obter a url do banco
-DATABASE_URL = os.getenv('DATABASE_URL')
-
 # criar a conexao com o banco
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(os.getenv('DATABASE_URL'))
 
 
 def create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
+    logging.info("Tabelas criadas!")
 
 
 def get_session() -> Session:
     return Session(engine)
+
+
+@event.listens_for(Engine, 'connect')
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    if type(dbapi_connection) is sqlite3.Connection:
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
